3c9e35033b1a38a53ac4417a735eef11e12ec41a
DaddyChucky <charles.delafontaine@gmail.com>
2020-09-14 19:10:35 +0000

    Auto commit

--- exercice.py
+++ exercice.py
@@ -10,6 +10,7 @@ def to_radians(angle_degs: float, angle_mins: float, angle_secs: float) -> float
     #return math.pi*(angle_degs+angle_mins/60+angle_secs/3600)/180
 
 def to_degrees(angle_rads: float) -> tuple:
+    print(angle_rads)
     degre = angle_rads/math.pi  *180
 
     tronc = degre - math.floor(degre)
