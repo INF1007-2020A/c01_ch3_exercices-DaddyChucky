9a323bb65067ca2816760ea4a151990439768e82
DaddyChucky <charles.delafontaine@gmail.com>
2020-09-14 18:21:27 +0000

    Auto commit

--- exercice.py
+++ exercice.py
@@ -12,8 +12,8 @@ def to_radians(angle_degs: float, angle_mins: float, angle_secs: float) -> float
 
 def to_degrees(angle_rads: float) -> tuple:
     deg = math.floor(angle_rads)
-    
-    return 0.0, 0.0, 0.0
+    return deg
+    #return 0.0, 0.0, 0.0
 
 
 def to_celsius(temperature: float) -> float:
