7f94ee2b14f467115166366b07a94621e8b63d2a
DaddyChucky <charles.delafontaine@gmail.com>
2020-09-14 18:48:52 +0000

    Auto commit

--- exercice.py
+++ exercice.py
@@ -19,7 +19,7 @@ def to_degrees(angle_rads: float) -> tuple:
     tronc = angle_mins-math.floor(angle_mins)
     angle_secs = tronc * 3600
 
-    return round(degre,2), round(angle_mins,2), round(angle_secs,2)
+    return round(degre,2), angle_mins, round(angle_secs,2)
 
 
 def to_celsius(temperature: float) -> float:
